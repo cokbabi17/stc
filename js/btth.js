2e9c1747734a5bd4fd95dda02cb4adb1e63fcd9c
cokbabi17 <74761609+cokbabi17@users.noreply.github.com>
2024-02-01 18:24:59 +0700

    Update btth.js

--- js/btth.js
+++ js/btth.js
@@ -1,5 +1,5 @@
 // Jumlah episode
-    var totalEpisodes = 82;
+    var totalEpisodes = 21;
 
     // Tangkap elemen tabs
     var tabsContainer = document.querySelector('.tabs');
@@ -8,15 +8,15 @@
     var currentPageUrl = window.location.href;
 
     // Buat tab-episode secara dinamis
-    for (var i = 80; i <= totalEpisodes; i++) {
-        var episodeUrl = '../p/btth-' + i + '.html';
+    for (var i = 1; i <= totalEpisodes; i++) {
+        var episodeUrl = 'https://iqiyoo.blogspot.com/p/rene-' + i + '.html';
 
         var tab = document.createElement('a');
         tab.setAttribute('href', episodeUrl);
         tab.textContent = ' ' + i;
 
         // Jika URL halaman saat ini cocok dengan URL tab, atur tab sebagai aktif
-        if (currentPageUrl.includes('/btth-' + i + '.html')) {
+        if (currentPageUrl.includes('/rene-' + i + '.html')) {
             tab.classList.add('active');
         }
 
